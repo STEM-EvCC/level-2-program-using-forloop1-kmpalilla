abe90ac2fc43154807b51c4414c566feaf3341d0
kmpalilla <kmpalilla@students.everettcc.edu>
2025-11-05 00:44:18 +0000

    Assignment Complete

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# STEM103_Level3_1-ForLoop
+## Class  
+STEM 103 Intro to Python  
+
+
+## Author  
+My name is Kaleb Palilla. I am learning Python in this intro course.  
+
+##  Program Using For Loop  
+I will be making a program utilizing for loops and using knowledge I have gained up until now.  
+This program will be using already provided data sets regarding NASA Missions that include name, year, and success/fail status.  
+I will show the total number of missions, number of successful missions, show the success rate (in a percentage),  
+and show the names of the missions launched before the year 2000. The data shown to user must match what was provided as well.  
--- main.py
+++ main.py
@@ -4,25 +4,34 @@
 # Show number of successful missions
 # Show success rate as a percentage
 # Show names of missions launched before the year 2000
+# Data shown to user must match what was given to keep uniformity
 
 mission_names = ['Apollo 11', 'Challenger', 'Curiosity Rover', 'Viking 1', 'Mars Pathfinder', 'Hubble Telescope', 'Apollo 13']
+# All missions by name
+
 mission_years = [1969, 1986, 2012, 1975, 1996, 1990, 1970]
+# All years missions happened
+
 mission_success = [True, False, True, True, True, True, False]
+# Success status of missions
 
-print("Total number of missions: " + str(len(mission_names)))
+print("Total number of missions: " + str(len(mission_names))) # Show the user the total amount of missions
+
 count = 0
-for x in mission_success:
-    if x:
+for x in mission_success: # Will get the amount of successful missions by finding all True entries
+    if x == True: # if x: also works. This is adding to the count for each True in the misssion_success variable
         count += 1
+print("Number of successful missions: " + str(count)) # Display to user number of successful mission based off the count in prior for loop
 
+success_rate = count / len(mission_success) * 100 # Acquire the percentage of the successful missions
 
-print("Number of successful missions: " + str(count))
-success_rate = count / len(mission_success) * 100
-print("Success rate: " + str(round(success_rate,2)) + "%")
+print("Success rate: " + str(round(success_rate,2)) + "%") # Show user the success rate in a percentage
+
 print("Missions launched before the year 2000: ")
-for x in range(len(mission_names)):
-   if mission_years[x] < 2000:
-    print("- " + mission_names[x])
+for x in range(len(mission_names)): # Using the range of the mission names to allow use in case more data is added
+   if mission_years[x] < 2000: # Checking through the mission years (x being the index) to see what missions are before year 2000
+    print("- " + mission_names[x]) # Using this format to match the provided output.
+    # Using x as the index it will pair mission names with mission years as long as the data correlate based off position in index
    
 
 
