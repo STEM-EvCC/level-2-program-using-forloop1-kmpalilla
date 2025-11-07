268cc9227f42a1e7aa7e2764768b66a717391c67
kmpalilla <kmpalilla@students.everettcc.edu>
2025-11-07 00:51:09 +0000

    Assignment Complete

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ I will be making a program utilizing for loops and using knowledge I have gained
 This program will be using already provided data sets regarding NASA Missions that include name, year, and success/fail status.  
 I will show the total number of missions, number of successful missions, show the success rate (in a percentage),  
 and show the names of the missions launched before the year 2000. The data shown to user must match what was provided as well.  
+This assignment will be using "len" a lot in order to obtain the data needed for desired output.  
--- main.py
+++ main.py
@@ -19,21 +19,22 @@ print("Total number of missions: " + str(len(mission_names))) # Show the user th
 
 count = 0
 for x in mission_success: # Will get the amount of successful missions by finding all True entries
-    if x == True: # if x: also works. This is adding to the count for each True in the misssion_success variable
-        count += 1
+    if x == True: # if x: also works, need to look up reasoning for why it works. This is adding to the count for each True in the misssion_success variable
+        count += 1 # Count goes up for every True the for loop finds
 print("Number of successful missions: " + str(count)) # Display to user number of successful mission based off the count in prior for loop
 
-success_rate = count / len(mission_success) * 100 # Acquire the percentage of the successful missions
+success_rate = count / len(mission_success) * 100 # Acquire the percentage of the successful missions by getting the number of entries in mission_success
+# Then dividing the count by the entries and multiply the result by 100 to get success_rate into desired format
 
-print("Success rate: " + str(round(success_rate,2)) + "%") # Show user the success rate in a percentage
+print("Success rate: " + str(round(success_rate,2)) + "%") # Show user the success rate in a percentage. Uses round to show only 2 decimal places for percentage
+# Could have used the string formatting from the provided example in the "Help! My code doesn't work" section instead to get desired output
 
 print("Missions launched before the year 2000: ")
-for x in range(len(mission_names)): # Using the range of the mission names to allow use in case more data is added
+for x in range(len(mission_names)): # Using the range of the mission names to allow use in case more data entries is added. Similar to fizzbuzz example
    if mission_years[x] < 2000: # Checking through the mission years (x being the index) to see what missions are before year 2000
     print("- " + mission_names[x]) # Using this format to match the provided output.
     # Using x as the index it will pair mission names with mission years as long as the data correlate based off position in index
+    # And keep looping until it checks all mission names
    
-
-
 
 
